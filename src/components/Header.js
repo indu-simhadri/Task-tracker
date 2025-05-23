18e1d13991014b6b8b0678c4fc2fe8fbeadc216b
Indrani Simhadri <simhadriindrani2410@gmail.com>
2025-05-23 17:03:57 +0530

    button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import Button from "./Button";
+import {useLocation} from 'react-router-dom';
 
 const Header = ({title = 'default title',onADDClick,showAddTask}) => {
+  const location  = useLocation();
   return (
     <header className = 'header'>
         <h1>{title}</h1>
+        {
+        location.pathname === '/'
+         &&
         <Button  color = {showAddTask ? 'red':'green'} text = {showAddTask ? 'Close':'Add'} onClick={onADDClick} />
+        }
     </header>
   )
 }
